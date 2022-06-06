c73d346d6d7eb569b9d9026cab5a376878b451c4
devwithpug <devwithpug@kakao.com>
2022-06-06 14:34:40 +0900

    init apollo server

--- apollo/server.js
+++ apollo/server.js
@@ -1,18 +1,14 @@
 import { ApolloServer, gql } from "apollo-server"
 
 const typeDefs = gql`
-
-
     type Query {
         text:String # GET /text
         hello:String # GET /hello
     }
-
-
 `
 
-const server = new ApolloServer({typeDefs})
+const server = new ApolloServer({ typeDefs })
 
-server.listen().then(({url}) => {
+server.listen().then(({ url }) => {
     console.log(`Running on ${url}`)
 })
