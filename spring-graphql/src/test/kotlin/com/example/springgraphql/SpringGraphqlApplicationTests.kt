b4def9ae3e0f8b577cc9c3d10e6224bb28783bec
devwithpug <devwithpug@kakao.com>
2022-06-06 16:57:37 +0900

    init: spring boot project

---
+++ spring-graphql/src/test/kotlin/com/example/springgraphql/SpringGraphqlApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.springgraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringGraphqlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
